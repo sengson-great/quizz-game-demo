edcc63b750e5c848561cbc3add9a0c269bf3e5a3
Seng Vengchhuong <leobomo5@gmail.com>
2026-05-29 00:04:30 +0700

    fix douplicated login

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -5,7 +5,3 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-
-Route::get('/login', function () {
-    return response()->json(['message' => 'Unauthenticated.'], 401);
-})->name('login');
