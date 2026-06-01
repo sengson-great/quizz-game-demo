72a4f7fa75e264130abf842cd20ffdb5f3841472
Seng Vengchhuong <leobomo5@gmail.com>
2026-06-02 00:09:09 +0700

    fix CI/CD

--- backend/tests/TestCase.php
+++ backend/tests/TestCase.php
@@ -6,5 +6,13 @@ use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
-    //
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        if (!file_exists(storage_path('oauth-private.key'))) {
+            \Illuminate\Support\Facades\Artisan::call('passport:keys', ['--force' => true]);
+        }
+    }
 }
+
